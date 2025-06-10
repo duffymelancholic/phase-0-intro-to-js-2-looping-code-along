8de5589a667d948948b2509322d43212762664cd
Nicholas obonyo abala <nicholasobonyo444@gmail.com>
2025-06-10 18:35:40 +0300

    All the loops are functional

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards (name , event){
+    const message = ["Thank you, Guadalupe, for the wonderful surprise gift!",
+       "Thank you, Ollie, for the wonderful surprise gift!",
+        "Thank you, Aki, for the wonderful surprise gift!",]
+    return message
+}
+function countDown (num){
+  while (num >= 0){
+console.log(num);
+    num--;
+  }
+}
